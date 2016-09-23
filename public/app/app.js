6fe26a23c7b71426d23ca3d03be5937aa4d5d4a4
Callan Morrison <callan.l.morrison@gmail.com>
2016-09-22 22:20:05 -0500

    boilerplate for class website

---
+++ public/app/app.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var app = angular.module("ClassWebsite", ["ngRoute", "ngAnimate"])
+
+
+/*
+app.config(function($routeProvider) {
+    $routeProvider
+        .when("/splash", {
+            templateUrl: "partials/splash.html",
+            controller:  "splashCtrl"
+        })
+        .when("/project/:projectName", {
+            templateUrl: "partials/project.html",
+            controller:  "projectCtrl"
+        })
+        .otherwise("/splash");
+});
+*/
