99b4c63a2daf053ea531fd54f8a2ee2d52c0ccf4
Dan Ventura <danwventura@icloud.com>
2016-10-09 10:49:13 -0500

    merge conflict in app.js fixed

--- public/app/app.js
+++ public/app/app.js
@@ -3,29 +3,16 @@
 var app = angular.module("ClassWebsite", ["ngRoute", "ngAnimate"])
 
 
-<<<<<<< HEAD
 
-=======
->>>>>>> master
 app.config(function($routeProvider) {
     $routeProvider
         .when("/splash", {
             templateUrl: "partials/splash.html",
             // controller:  "splashCtrl"
         })
-<<<<<<< HEAD
-        .when("/us", {
-            templateUrl: "partials/facesOne.html",
-            controller:  "FacesCtrl"
-=======
         .when("/people", {
             templateUrl: "partials/facesOne.html",
-            // controller:  "projectCtrl"
->>>>>>> master
+            controller:  "FacesCtrl"
         })
         .otherwise("/people");
 });
-<<<<<<< HEAD
-
-=======
->>>>>>> master
