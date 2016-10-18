56750126ccc2871057d0a5be79ccdbd85c526954
VikingPaul <wip321@yahoo.com>
2016-10-17 20:03:41 -0500

    the card flips when user clicks on a picture. if they click on a picture too fast after the previous clicked picture then the card might not flip. speeding up the card flip in CSS will

--- public/app/controllers/Faces.Ctrl.js
+++ public/app/controllers/Faces.Ctrl.js
@@ -18,6 +18,7 @@ app.controller("FacesCtrl", function($scope, JSONFactory){
   $scope.showStudentDetails = function(student){
     if (student.firstName === $scope.card.firstName) {
       $(`#${$scope.card.firstName}`).removeClass("shiny")
+      animateCard();
       $scope.card = {
         image: "image/turdface.png",
         bio: "const EveningCohort3={ <br/> &nbsp;dayJobs: true,<br/> &nbsp;coffeeConsumed: '36,823lb', <br/> &nbsp;conqueredValleyOfDespair: true <br/>}",
@@ -26,9 +27,17 @@ app.controller("FacesCtrl", function($scope, JSONFactory){
       } 
     } else {
       $(`#${$scope.card.firstName}`).removeClass("shiny")
+      animateCard();
       $scope.card = student;
       $scope.card.tech = false;
       $(`#${$scope.card.firstName}`).addClass("shiny")
     };
   };
+  function animateCard() {
+    $("#class-card").addClass("animated flip");
+    $('#class-card').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', () => {
+      $("#class-card").removeClass("animated flip");
+    });
+  };
+
 });
