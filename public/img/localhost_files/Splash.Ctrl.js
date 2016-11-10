f15ca71c08ba8cb7ace68b33ca70f3614f4364f7
Callan Morrison <callan.l.morrison@gmail.com>
2016-11-10 12:14:45 -0600

    pulled unneccessary files

--- public/img/localhost_files/Splash.Ctrl.js
+++
@@ -1,9 +0,0 @@
-app.controller("SplashCtrl", function($scope, deviceDetector, $location){
-
-  var deviceInfo = this;
-  deviceInfo.data = deviceDetector;
-  deviceInfo.allData = JSON.stringify(deviceInfo, null, 2);
-  if(deviceInfo.data.isMobile() && !deviceInfo.data.isTablet()) {
-    $location.url('/mobile')
-  }
-});
--- public/img/localhost_files/Technologies.Ctrl.js
+++
@@ -1,9 +0,0 @@
-app.controller('TechnologiesCtrl', function($scope, JSONFactory) {
-  $scope.technologies = {};
-  JSONFactory.getTechnologies().then((techs) => {
-    $(document).ready(function(){
-      $('.tooltipped').tooltip({delay: 50});
-    });
-    $scope.technologies = techs;
-  })
-})
